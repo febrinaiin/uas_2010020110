044913063765129e91335413a67fd5c09f036637
febrinaiin <nafebrina789@gmail.com>
2023-01-20 10:50:53 +0800

    UAS

---
+++ android/app/src/main/kotlin/com/example/uas_febrina/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uas_febrina
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'menu.dart';
+import 'persegi.dart';
+import 'lingkaran.dart';
+import 'profil.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "UAS.P.Mobile",
+      initialRoute: "/",
+      routes: {
+        "/": (context) => const menuAwal(),
+        hitungPersegi.routeName: (context) => const hitungPersegi(),
+        hitungLingkaran.routeName: (context) => const hitungLingkaran(),
+        profileDev.routeName: (context) => const profileDev(),
+      },
+    ),
+  );
+}
